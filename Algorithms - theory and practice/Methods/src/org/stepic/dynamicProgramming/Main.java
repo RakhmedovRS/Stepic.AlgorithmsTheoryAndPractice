e79e87f6904dc2b062d410d8484cc02efd9ce47a
Руслан Рахмедов <RSRakhmedov.SBT@sberbank.ru>
2018-06-27 15:08:20 +0300

    Новое решение

--- Algorithms - theory and practice/Methods/src/org/stepic/dynamicProgramming/Main.java
+++ Algorithms - theory and practice/Methods/src/org/stepic/dynamicProgramming/Main.java
@@ -1,22 +1,17 @@
 package org.stepic.dynamicProgramming;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.Scanner;
 
 public class Main
 {
-	private static int[] inputValues = getInputValues();
-	private static List<List<Integer>> results = new LinkedList<>();
+	private static ArrayList<ArrayList<Integer>> results = new ArrayList<>();
 	private static int minValue = Integer.MAX_VALUE;
 	private static int maxValue = Integer.MIN_VALUE;
+	private static int[] inputValues = getInputValues();
 	private static boolean needRestoreMax = false;
-	private static int counterRestoreMax = 0;
-	private static int counterGetLargestList1 = 0;
-	private static int counterGetLargestList2 = 0;
-	private static int counterRemoveListByLength = 0;
-	private static int counterRestoreMaxCall = 0;
-	private static int counterGetLargestList1Call = 0;
-	private static int counterGetLargestList2Call = 0;
-	private static int counterRemoveListByLengthCall = 0;
 
 	private static int[] getInputValues()
 	{
@@ -38,10 +33,8 @@ public class Main
 	{
 		maxValue = Integer.MIN_VALUE;
 
-		counterRestoreMaxCall++;
 		for (List<Integer> list : results)
 		{
-			counterRestoreMax++;
 			if (list != null)
 			{
 				int index = list.listIterator(list.size()).previous();
@@ -55,27 +48,58 @@ public class Main
 		needRestoreMax = false;
 	}
 
-	private static List<Integer> getLargestList()
+	@SuppressWarnings("unchecked")
+	private static void addToResults(ArrayList<Integer> newList)
 	{
-		List<Integer> largestList = null;
-		counterGetLargestList1Call++;
-		for (List<Integer> list : results)
+		Object[] oResults = results.toArray();
+
+		if (oResults.length == 0 || ((ArrayList<Integer>) oResults[0]).size() >= newList.size())
 		{
-			counterGetLargestList1++;
-			if (largestList == null)
+			results.add(0, newList);
+		}
+		else if (newList.size() >= ((ArrayList<Integer>) oResults[oResults.length - 1]).size())
+		{
+			results.add(newList);
+		}
+		else
+		{
+			int left = 0;
+			int right = oResults.length - 1;
+			int middle = (left + right) / 2;
+
+			while (left < right)
 			{
-				largestList = list;
+				middle = (left + right) / 2;
+
+				if (newList.size() > ((ArrayList<Integer>) oResults[middle]).size())
+				{
+					left = middle + 1;
+				}
+				else
+				{
+					right = middle - 1;
+				}
+			}
+
+			if (newList.size() < ((ArrayList<Integer>) oResults[left]).size())
+			{
+				results.add(left, newList);
+			}
+			else if ((newList.size() >= ((ArrayList<Integer>) oResults[left]).size())
+				&& (newList.size() <= ((ArrayList<Integer>) oResults[middle]).size()))
+			{
+				results.add(middle, newList);
+			}
+			else if ((newList.size() >= ((ArrayList<Integer>) oResults[middle]).size())
+				&& (newList.size() <= ((ArrayList<Integer>) oResults[right]).size()))
+			{
+				results.add(middle + 1, newList);
 			}
 			else
 			{
-				if (list.size() > largestList.size())
-				{
-					largestList = list;
-				}
+				results.add(right + 1, newList);
 			}
 		}
-
-		return largestList;
 	}
 
 	private static List<Integer> getLargestList(int largestValue)
@@ -83,10 +107,8 @@ public class Main
 		List<Integer> largestList = null;
 		int maxLength = Integer.MIN_VALUE;
 
-		counterGetLargestList2Call++;
 		for (List<Integer> list : results)
 		{
-			counterGetLargestList2++;
 			int largestListElement = inputValues[list.listIterator(list.size()).previous()];
 			if (largestListElement <= largestValue
 				&& list.size() > maxLength)
@@ -98,29 +120,59 @@ public class Main
 		return largestList;
 	}
 
+	@SuppressWarnings("unchecked")
 	private static void removeListByLength(int length)
 	{
-		ListIterator<List<Integer>> listIterator = results.listIterator();
+		Object[] oResults = results.toArray();
 
-		counterRemoveListByLengthCall++;
-		while (listIterator.hasNext())
+		int left = 0;
+		int right = oResults.length - 1;
+		int middle = (left + right) / 2;
+
+		while (left < right)
 		{
-			counterRemoveListByLength++;
-			List<Integer> list = listIterator.next();
-			if (list != null && list.size() == length)
+			middle = (left + right) / 2;
+
+			if (length > ((ArrayList<Integer>) oResults[middle]).size())
 			{
-				if (list.listIterator(list.size()).previous() == maxValue)
-				{
-					needRestoreMax = true;
-				}
-				listIterator.remove();
+				left = middle + 1;
 			}
+			else
+			{
+				right = middle - 1;
+			}
+		}
+
+		if (length < ((ArrayList<Integer>) oResults[left]).size())
+		{
+			middle = left;
+		}
+		else if ((length >= ((ArrayList<Integer>) oResults[middle]).size())
+			&& (length <= ((ArrayList<Integer>) oResults[right]).size()))
+		{
+			middle++;
+		}
+		else if (length > ((ArrayList<Integer>) oResults[left]).size())
+		{
+			middle = right + 1;
+		}
+
+		ArrayList<Integer> list;
+		while (middle < results.size() && results.get(middle).size() != length + 1)
+		{
+
+			list = results.get(middle);
+			if (list.get(list.size() - 1) == maxValue)
+			{
+				needRestoreMax = true;
+			}
+			results.remove(middle);
 		}
 	}
 
 	private static void printResults()
 	{
-		List<Integer> largestList = getLargestList();
+		List<Integer> largestList = results.get(results.size() - 1);
 		StringBuilder stringBuilder = new StringBuilder();
 
 		ListIterator<Integer> listIterator = largestList.listIterator(largestList.size());
@@ -142,25 +194,25 @@ public class Main
 		{
 			if (inputValues[i] < minValue)
 			{
-				List<Integer> newList = new LinkedList<>();
+				ArrayList<Integer> newList = new ArrayList<>();
 				newList.add(i);
-				results.add(newList);
+				addToResults(newList);
 				minValue = inputValues[i];
 			}
 			else if (inputValues[i] > maxValue)
 			{
-				List<Integer> newList = new LinkedList<>();
-				newList.addAll(getLargestList());
+				ArrayList<Integer> newList = new ArrayList<>();
+				newList.addAll(results.get(results.size() - 1));
 				newList.add(i);
-				results.add(newList);
+				addToResults(newList);
 				maxValue = inputValues[i];
 			}
 			else
 			{
-				List<Integer> newList = new LinkedList<>(getLargestList(inputValues[i]));
+				ArrayList<Integer> newList = new ArrayList<>(getLargestList(inputValues[i]));
 				newList.add(i);
 				removeListByLength(newList.size());
-				results.add(newList);
+				addToResults(newList);
 				if (needRestoreMax)
 				{
 					restoreMax();
@@ -170,9 +222,5 @@ public class Main
 
 		printResults();
 		System.out.println(String.format("Time spend: %s ms", System.currentTimeMillis() - startTime));
-		System.out.println(String.format("counterRestoreMax: %s , calls: %s", counterRestoreMax, counterRestoreMaxCall));
-		System.out.println(String.format("counterGetLargestList1: %s , calls: %s", counterGetLargestList1, counterGetLargestList1Call));
-		System.out.println(String.format("counterGetLargestList2: %s , calls: %s", counterGetLargestList2, counterGetLargestList2Call));
-		System.out.println(String.format("counterRemoveListByLength: %s , calls: %s", counterRemoveListByLength, counterRemoveListByLengthCall));
 	}
 }
