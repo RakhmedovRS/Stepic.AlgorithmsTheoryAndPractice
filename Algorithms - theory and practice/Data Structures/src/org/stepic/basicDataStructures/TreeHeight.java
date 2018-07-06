870554610cd94ecb8d436e4fc6791269e64cebfd
rassoll <rassoll32@gmail.com>
2018-07-06 22:05:00 +0300

    Добавлено решение задачи "Высота дерева"

---
+++ Algorithms - theory and practice/Data Structures/src/org/stepic/basicDataStructures/TreeHeight.java
@@ -0,0 +1,44 @@
+package org.stepic.basicDataStructures;
+
+import java.util.Scanner;
+
+public class TreeHeight
+{
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int root = -1;
+		int[] nodes = new int[scanner.nextInt()];
+
+		for (int i = 0; i < nodes.length; i++)
+		{
+			int node = scanner.nextInt();
+			root = node == -1 ? i : root;
+			nodes[i] = node;
+		}
+		scanner.close();
+
+		int maxHeight = 0;
+
+		for (int node : nodes)
+		{
+			int parent = node;
+			int height = 1;
+			while (true)
+			{
+				if (parent != -1)
+				{
+					height++;
+					parent = nodes[parent];
+				}
+				else
+				{
+					maxHeight = height > maxHeight ? height : maxHeight;
+					break;
+				}
+			}
+		}
+
+		System.out.println(maxHeight);
+	}
+}
