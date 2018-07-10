f7b30e05d27fc4b6655783d26283d6cce981ffdb
Руслан Рахмедов <RSRakhmedov.SBT@sberbank.ru>
2018-07-10 15:47:54 +0300

    Добавлено решение задачи "Максимум в скользящем окне"

---
+++ Algorithms - theory and practice/Data Structures/src/org/stepic/basicDataStructures/MaximumInTheSlidingWindow.java
@@ -0,0 +1,71 @@
+package org.stepic.basicDataStructures;
+
+import java.util.Scanner;
+
+public class MaximumInTheSlidingWindow
+{
+	private static int getMaxValueIndex(int[] values, int startPos, int endPos)
+	{
+		int maxValueIndex = startPos;
+		for (int i = startPos; i <= endPos; i++)
+		{
+			maxValueIndex = values[i] > values[maxValueIndex] ? i : maxValueIndex;
+		}
+		return maxValueIndex;
+	}
+
+	public static void main(String[] args)
+	{
+		Scanner scanner = new Scanner(System.in);
+		int valuesCount = scanner.nextInt();
+		int[] values = new int[valuesCount];
+		for (int i = 0; i < valuesCount; i++)
+		{
+			values[i] = scanner.nextInt();
+		}
+		int windowSize = scanner.nextInt();
+
+		if (valuesCount == windowSize)
+		{
+			System.out.println(values[getMaxValueIndex(values, 0, values.length - 1)]);
+		}
+		else if (windowSize == 1)
+		{
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int element : values)
+			{
+				stringBuilder.append(element);
+				stringBuilder.append(" ");
+			}
+			System.out.println(stringBuilder.toString());
+		}
+		else
+		{
+			int left = 0;
+			int right = windowSize - 1;
+			int maxValueIndex = getMaxValueIndex(values, left, right);
+
+			StringBuilder stringBuilder = new StringBuilder();
+			while (right < values.length)
+			{
+				if (left > maxValueIndex)
+				{
+					maxValueIndex = getMaxValueIndex(values, left, right);
+				}
+
+				if (values[right] > values[maxValueIndex])
+				{
+					maxValueIndex = right;
+				}
+
+				stringBuilder.append(values[maxValueIndex]);
+				stringBuilder.append(" ");
+
+				left++;
+				right++;
+			}
+
+			System.out.println(stringBuilder.toString());
+		}
+	}
+}
