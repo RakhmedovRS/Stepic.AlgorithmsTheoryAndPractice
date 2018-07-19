2fa18c59c85309173ca6a0abb63328967ffdaab8
Руслан Рахмедов <RSRakhmedov.SBT@sberbank.ru>
2018-07-19 16:59:36 +0300

    Доработка задачи "Поиск образца в тексте"

--- Algorithms - theory and practice/Data Structures/src/org/stepic/hashTables/Main.java
+++ Algorithms - theory and practice/Data Structures/src/org/stepic/hashTables/Main.java
@@ -1,25 +1,27 @@
 package org.stepic.hashTables;
 
+import java.util.LinkedList;
 import java.util.Scanner;
 
 public class Main
 {
-	private static final int DIVIDER = 1_000_000_007;
-	private static final int BASE = 263;
+	private static final int DIVIDER = 1007;
+	private static int base;
 	private static long power;
+	private static long subStringHashCode;
 
 	private static long pow(int pow)
 	{
 		long result = 1;
 		for (int i = 0; i < pow; i++)
 		{
-			result = (result * BASE) % DIVIDER;
+			result = (result * base) % DIVIDER;
 		}
 
 		return result;
 	}
 
-	private static int hashCode(String string)
+	private static long hashCode(String string)
 	{
 		long hashCode = 0;
 		int i = 0;
@@ -29,26 +31,27 @@ public class Main
 			i++;
 		}
 
-		return (int) hashCode;
+		return hashCode;
 	}
 
-	private static int hashCode(char[] textChars, int i, int oldHashCode, int patternLength)
+	private static long hashCode(char[] textChars, int i, int patternLength)
 	{
 		long hashCode = 0;
 
-		if (i != 0)
+		if (i != textChars.length - patternLength)
 		{
-			hashCode = (oldHashCode - textChars[i - 1]) / BASE + (textChars[i - 1 + patternLength] * power) % DIVIDER;
+			hashCode = ((subStringHashCode - (textChars[i + patternLength] * power) % DIVIDER) + DIVIDER) % DIVIDER;
+			hashCode = (hashCode * base + textChars[i]) % DIVIDER;
 		}
 		else
 		{
-			for (int pos = i; pos < i + patternLength; pos++)
+			for (int pow = 0, pos = i; pos < i + patternLength; pos++, pow++)
 			{
-				hashCode = (((hashCode + (textChars[pos] * pow(pos))) % DIVIDER) + DIVIDER) % DIVIDER;
+				hashCode = (hashCode + (textChars[pos] * pow(pow)) % DIVIDER) % DIVIDER;
 			}
 		}
 
-		return (int) hashCode;
+		return hashCode;
 	}
 
 	public static void main(String[] args)
@@ -57,19 +60,21 @@ public class Main
 		{
 			String pattern = scanner.next();
 			String text = scanner.next();
+
+			base = pattern.length() == 1 ? 1 : pattern.length() - 1;
 			power = pow(pattern.length() - 1);
-			int subStringHashCode = 0;
-			int patternHashCode = hashCode(pattern);
+
+			long patternHashCode = hashCode(pattern);
 
 			char[] textChars = text.toCharArray();
 			char[] patternChars = pattern.toCharArray();
 
-			StringBuilder stringBuilder = new StringBuilder();
+			LinkedList<String> answer = new LinkedList<>();
 
-			int i = 0;
-			while (i + pattern.length() <= textChars.length)
+			int i = textChars.length - pattern.length();
+			while (i >= 0)
 			{
-				subStringHashCode = hashCode(textChars, i, subStringHashCode, pattern.length());
+				subStringHashCode = hashCode(textChars, i, pattern.length());
 
 				if (patternHashCode == subStringHashCode)
 				{
@@ -84,14 +89,13 @@ public class Main
 
 					if (equals)
 					{
-						stringBuilder.append(i);
-						stringBuilder.append(" ");
+						answer.addFirst(String.valueOf(i));
 					}
 				}
-				i++;
+				i--;
 			}
 
-			System.out.println(stringBuilder.toString());
+			System.out.println(answer.toString().replaceAll("\\[", "").replaceAll("]", "").replaceAll(",", ""));
 		}
 	}
 }
